9db46612e3864e60d311205fce60119422d871b3
Dang Hoang Ha <ha.dh290702@gmail.com>
2025-03-11 13:40:28 +0200

    Refactor queries

--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -1,27 +1,15 @@
 'use server';
 
-import { cookies } from 'next/headers';
-import { Account, Client, Databases, Query } from 'node-appwrite';
-import { AUTH_COOKIE } from '../auth/constants';
+import { Query } from 'node-appwrite';
+
 import { DATABASE_ID, MEMBERS_ID, WORKSPACES_ID } from '@/config';
 import { getMember } from '../members/utils';
 import { Workspace } from './type';
+import { createSessionClient } from '@/lib/appwrite';
 
 export const getWorkspaces = async () => {
         try {
-                const client = new Client()
-                        .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-                        .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
-
-                const session = await cookies().get(AUTH_COOKIE);
-
-                if (!session) {
-                        return { documents: [], total: 0 };
-                }
-
-                client.setSession(session.value);
-                const databases = new Databases(client);
-                const account = new Account(client);
+                const { databases, account } = await createSessionClient();
                 const user = await account.get();
                 const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
                         Query.equal('userId', user.$id),
@@ -49,19 +37,7 @@ interface GetWorkspaceProps {
 }
 export const getWorkspace = async ({ workspaceId }: GetWorkspaceProps) => {
         try {
-                const client = new Client()
-                        .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-                        .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
-
-                const session = await cookies().get(AUTH_COOKIE);
-
-                if (!session) {
-                        return { documents: [], total: 0 };
-                }
-
-                client.setSession(session.value);
-                const databases = new Databases(client);
-                const account = new Account(client);
+                const { databases, account } = await createSessionClient();
                 const user = await account.get();
 
                 const member = await getMember({ databases, userId: user.$id, workspaceId });
