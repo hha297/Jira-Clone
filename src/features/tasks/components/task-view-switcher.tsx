1c8bfaf409fe56a99ae757989e1fc63f3a2c15a6
Dang Hoang Ha <ha.dh290702@gmail.com>
2025-03-25 10:50:08 +0200

    Data calendar

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -16,6 +16,7 @@ import { columns } from './column';
 import { DataKanban } from './data-kanban';
 import { TaskStatus } from '../type';
 import { useBulkUpdateTask } from '../api/use-bulk-update-task';
+import { DataCalendar } from './data-calendar';
 
 export const TaskViewSwitcher = () => {
         const [{ status, assigneeId, projectId, dueDate }] = useTaskFilters();
@@ -79,8 +80,8 @@ export const TaskViewSwitcher = () => {
                                                                 onChange={onKanbanChange}
                                                         />
                                                 </TabsContent>
-                                                <TabsContent value="calendar" className="mt-0">
-                                                        {JSON.stringify(tasks)}
+                                                <TabsContent value="calendar" className="mt-0 h-full pb-4">
+                                                        <DataCalendar data={tasks?.documents ?? []} />
                                                 </TabsContent>
                                         </>
                                 )}
